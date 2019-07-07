0d61f78a937e009eec5d8e5c971af7c3d3835cde
trung.caosy <trung.cao@recomot.co.jp>
2019-07-08 00:25:23 +0700

    create main screen

--- app/src/main/java/com/example/trungcaosy/bat_pagoda/presentation/ui/main/MainActivity.java
+++ app/src/main/java/com/example/trungcaosy/bat_pagoda/presentation/ui/main/MainActivity.java
@@ -5,17 +5,26 @@ import android.support.v4.view.ViewPager;
 
 import com.example.trungcaosy.bat_pagoda.R;
 import com.example.trungcaosy.bat_pagoda.base.BaseActivity;
+import com.example.trungcaosy.bat_pagoda.presentation.custom_view.indicator.ViewPagerIndicator;
+import com.example.trungcaosy.bat_pagoda.presentation.ui.main.adapter.ImagePagerAdapter;
 import com.example.trungcaosy.bat_pagoda.presentation.ui.main.di.MainContract;
 import com.example.trungcaosy.bat_pagoda.presentation.ui.main.di.MainPresenter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
+import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
 
 public class MainActivity extends BaseActivity<MainContract.ViewContract, MainContract.PresenterContract>
         implements MainContract.ViewContract {
     @BindView(R.id.vpMain)
-    ViewPager vpMain;
+    AutoScrollViewPager vpMain;
+
+    @BindView(R.id.view_pager_indicator)
+    ViewPagerIndicator indicator;
 
     @Inject
     MainPresenter mMainPresenter;;
@@ -34,7 +43,33 @@ public class MainActivity extends BaseActivity<MainContract.ViewContract, MainCo
 
     @Override
     protected void onViewReady(Bundle bundle) {
+        List<Integer> imageList = new ArrayList<>();
+        imageList.add(R.drawable.img_vp_1);
+        imageList.add(R.drawable.img_vp_2);
+        imageList.add(R.drawable.img_vp_3);
+        imageList.add(R.drawable.img_vp_4);
 
+        vpMain.setAdapter(new ImagePagerAdapter(this, imageList).setInfiniteLoop(false));
+      //  vpMain.setOnPageChangeListener(new MyOnPageChangeListener());
+
+        vpMain.setInterval(3000);
+        vpMain.startAutoScroll();
+        vpMain.setCurrentItem(0);
+
+        indicator.setupWithViewPager(vpMain);
+        //  viewPagerIndicator.addOnPageChangeListener(mOnPageChangeListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        vpMain.startAutoScroll();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        vpMain.stopAutoScroll();
     }
 
     @Override
@@ -50,5 +85,22 @@ public class MainActivity extends BaseActivity<MainContract.ViewContract, MainCo
     @Override
     public void showError(String title, String message) {
 
+    }
+
+    private class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/trungcaosy/bat_pagoda/presentation/ui/main/adapter/ImagePagerAdapter.java
@@ -0,0 +1,75 @@
+package com.example.trungcaosy.bat_pagoda.presentation.ui.main.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+public class ImagePagerAdapter extends RecyclingPagerAdapter {
+
+    private Context context;
+    private List<Integer> imageIdList;
+
+    private int           size;
+    private boolean       isInfiniteLoop;
+
+    public ImagePagerAdapter(Context context, List<Integer> imageIdList) {
+        this.context = context;
+        this.imageIdList = imageIdList;
+        this.size = imageIdList.size();
+        isInfiniteLoop = false;
+    }
+
+    @Override
+    public int getCount() {
+        // Infinite loop
+        return isInfiniteLoop ? Integer.MAX_VALUE : imageIdList.size();
+    }
+
+    /**
+     * get really position
+     *
+     * @param position
+     * @return
+     */
+    private int getPosition(int position) {
+        return isInfiniteLoop ? position % size : position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup container) {
+        ViewHolder holder;
+        if (view == null) {
+            holder = new ViewHolder();
+            view = holder.imageView = new ImageView(context);
+            ((ImageView) view).setScaleType(ImageView.ScaleType.CENTER_CROP);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder)view.getTag();
+        }
+        holder.imageView.setImageResource(imageIdList.get(getPosition(position)));
+        return view;
+    }
+
+    private static class ViewHolder {
+
+        ImageView imageView;
+    }
+
+    /**
+     * @return the isInfiniteLoop
+     */
+    public boolean isInfiniteLoop() {
+        return isInfiniteLoop;
+    }
+
+    /**
+     * @param isInfiniteLoop the isInfiniteLoop to set
+     */
+    public ImagePagerAdapter setInfiniteLoop(boolean isInfiniteLoop) {
+        this.isInfiniteLoop = isInfiniteLoop;
+        return this;
+    }
+}
